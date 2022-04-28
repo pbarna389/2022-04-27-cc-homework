f037394a8a1465fe41138043f657acada458341b
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-04-28 10:07:38 +0200

    FLIPPING WORKS DIFFERENTLY FOR ODDS

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -7,25 +7,26 @@ const mainSectionComponent = (data) => {
         </section>`
 };
 
-const movieDetailsComponent = (title, year, synops, id) => {
+const movieDetailsComponent = (title, evenOrOdd, year, synops, id) => {
     return `
-        <div class="cards-inner flipCardY" data-id=${id}>
+        <div class="cards-inner" data-id=${id}>
             <div class="cards-front" data-id=${id}>
                 <h2 data-id=${id}>${title}</h2>
             </div>
-            <div class="cards-back" data-id=${id}>
+            <div class="cards-back ${evenOrOdd}" data-id=${id}>
                 <h3 data-id=${id}>${year}</h3>
                 <h4 data-id=${id}>${synops}</h4>
             </div>
         </div>`
 };
 
+
 const initialize = () => {
     const rootID = document.querySelector('#root');
     const movieCards = movies.cards;
 
     movieCards.forEach((element, index) => {
-        rootID.insertAdjacentHTML('beforeend', mainSectionComponent(movieDetailsComponent(element.title, element.sub, element.text, index + 1)));
+        index % 2 === 0 ? rootID.insertAdjacentHTML('beforeend', mainSectionComponent(movieDetailsComponent(element.title, "even", element.sub, element.text, index + 1))) : rootID.insertAdjacentHTML('beforeend', mainSectionComponent(movieDetailsComponent(element.title, "odd", element.sub, element.text, index + 1)));
     });
 
     const cardsAll = [...document.querySelectorAll('.cards-inner')];
@@ -36,12 +37,13 @@ const initialize = () => {
         const dataId = target.getAttribute('data-id');
         const targetData = document.querySelector(`[data-id="${dataId}"]`);
         console.log(targetData, dataId);
-        cardsAll.map((card) => card === targetData ? card.classList.toggle('flipCardY') : card.classList.add('flipCardY'));
+        cardsAll.map((card, index) => index % 2 === 0 && card === targetData ? card.classList.toggle('flipCardY')
+            : index % 2 !== 0 && card === targetData ? card.classList.toggle('flipCardX')
+                : index % 2 !== 0 && card !== targetData ? card.classList.remove('flipCardX')
+                    : card.classList.remove('flipCardY'));
     };
 
     cardsAll.map(element => element.addEventListener('click', clickForBorder));
 };
 
 window.addEventListener('load', initialize);
-
-// cardsAll.map((card, index) => index % 2 === 0 && card === targetData ? card.classList.toggle('flipCardY') && card.classList.remove('flipCardX') : index % 2 !== 0 && card === targetData ? card.classList.toggle('flipCardX') && card.classList.remove('flipCardY') : card.classList.add('flipCardY') && card.classList.add('flipCardX'));
